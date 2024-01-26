573e6ac5ff711cc6f96bd64a2e1c98f85e8de52c
Ngoc Duy Anh Nguyen <duythedeveloper@gmail.com>
2024-01-26 14:23:51 -0500

    demo redirect for cbs

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,19 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  redirects() {
+    return [
+      {
+        source: "/english",
+        destination: "https://cbs-executive.dk/en/programs/summer-school",
+        permanent: true,
+      },
+      {
+        source: "/danish",
+        destination: "https://cbs-executive.dk/da/programs/sommerskole",
+        permanent: true,
+      },
+    ];
+  },
+};
 
 export default nextConfig;
